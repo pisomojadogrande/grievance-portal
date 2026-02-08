2148be3e3af355b37ad8f5469d6b16bbb3a1d266
pisomojadogrande <nobody@nowhere.com>
2026-02-08 22:22:16 +0000

    Fix lambda handler export syntax
    - Change from module.exports to ESM export
    - Removes CommonJS warning during build

--- server/lambda.ts
+++ server/lambda.ts
@@ -1,4 +1,4 @@
 import serverlessExpress from '@codegenie/serverless-express';
 import app from './index';
 
-module.exports.handler = serverlessExpress({ app });
+export const handler = serverlessExpress({ app });
